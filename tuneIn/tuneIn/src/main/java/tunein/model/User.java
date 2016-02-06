ea26f2dc2443e18a20e6fd383597155e2108d04b
nimisha-srinivasa <nimishasrinivasa174@gmail.com>
2016-02-06 03:36:15 -0800

    adding the user model

---
+++ tuneIn/tuneIn/src/main/java/tunein/model/User.java
@@ -0,0 +1,37 @@
+package tunein.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.*;
+
+@XmlRootElement
+public class User {
+	private String userId;
+	private String userName;
+	private String emailId;
+	private String password;
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
