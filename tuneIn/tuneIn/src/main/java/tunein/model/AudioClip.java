dda99c37db7c4d35a0fc290688bdc630920b9e00
nimisha-srinivasa <nimishasrinivasa174@gmail.com>
2016-02-06 03:33:27 -0800

    Adding fields to audio

--- tuneIn/tuneIn/src/main/java/tunein/model/AudioClip.java
+++ tuneIn/tuneIn/src/main/java/tunein/model/AudioClip.java
@@ -1,4 +1,5 @@
 package tunein.model;
+
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.*;
 
@@ -6,7 +7,8 @@ import java.util.*;
 // JAX-RS supports an automatic mapping from JAXB annotated class to XML and JSON
 public class AudioClip {
   private String keyname;
-  private String value; //changing to blob in future
+  private String audio; //changing to blob in future
+  private String image; //changing to blob in future
   private Date date;
 
   
@@ -14,28 +16,43 @@ public class AudioClip {
 
   }
 
-  public AudioClip(String keyname, String value, Date date){
+  public AudioClip(String keyname, String audio, Date date){
   	this.keyname=keyname;
-  	this.value=value;
+  	this.audio=audio;
   	this.date=date;
   }
 
-  public String getKeyname() {
-    return keyname;
-  }
-  public void setKeyname(String keyname) {
-    this.keyname = keyname;
-  }
-  public String getValue() {
-    return value;
-  }
-  public void setValue(String value) {
-    this.value = value;
-  }
-  public Date getDate() {
-    return date;
-  }
-  public void setDate(Date date) {
-    this.date = date;
-  }
+	public String getKeyname() {
+		return keyname;
+	}
+	
+	public void setKeyname(String keyname) {
+		this.keyname = keyname;
+	}
+	
+	public String getAudio() {
+		return audio;
+	}
+	
+	public void setAudio(String audio) {
+		this.audio = audio;
+	}
+	
+	public String getImage() {
+		return image;
+	}
+	
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+public Date getDate() {
+	return date;
+}
+
+public void setDate(Date date) {
+	this.date = date;
+}
+
+  
 } 
